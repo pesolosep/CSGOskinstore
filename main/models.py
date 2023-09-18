1ff9b258b983359b2e3b68baa2636ffacbb2ebbe
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2023-09-18 16:01:06 +0700

    update README.md

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Product(models.Model):
-    item = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     type = models.TextField()
     price = models.IntegerField()
     amount =  models.IntegerField()
